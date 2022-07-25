b01918deb734a31a6d39dc32a700ec4a901b15fb
woxtu <woxtup@gmail.com>
2022-07-26 03:16:39 +0900

    Make the activity indicator simple

--- HackerNews/Components/ActivityIndicatorView.swift
+++ HackerNews/Components/ActivityIndicatorView.swift
@@ -8,35 +8,16 @@
 
 import SwiftUI
 
-struct ActivityIndicatorView: View {
+struct ActivityIndicatorView: UIViewRepresentable {
     let style: UIActivityIndicatorView.Style
     let color: UIColor
-    @State private var size: CGSize = .zero
 
-    var body: some View {
-        GeometryReader { geometry in
-            InnerView(style: self.style, color: self.color, parentSize: geometry.size, size: self.$size)
-        }
-        .frame(height: size.height)
+    func makeUIView(context: Context) -> UIStackView {
+        let activityIndicatorView = UIActivityIndicatorView(style: style)
+        activityIndicatorView.color = color
+        activityIndicatorView.startAnimating()
+        return UIStackView(arrangedSubviews: [activityIndicatorView])
     }
 
-    struct InnerView: UIViewRepresentable {
-        let style: UIActivityIndicatorView.Style
-        let color: UIColor
-        let parentSize: CGSize
-        @Binding var size: CGSize
-
-        func makeUIView(context: Context) -> UIActivityIndicatorView {
-            let uiView = UIActivityIndicatorView(style: style)
-            uiView.color = color
-            uiView.startAnimating()
-            return uiView
-        }
-
-        func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
-            DispatchQueue.main.async {
-                self.size = uiView.sizeThatFits(self.parentSize)
-            }
-        }
-    }
+    func updateUIView(_ uiView: UIStackView, context: Context) {}
 }
